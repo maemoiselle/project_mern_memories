e056afb9a2100e5bf22d2fe1c0ad03ac435e9258
Alishia Patricio <alishia.m.patricio@gmail.com>
2022-03-07 18:04:12 -0500

    added style dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
+
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
 import memories from './images/memories.png';
@@ -30,7 +30,6 @@ const App = () => {
               <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
---
+++ client/src/theme/theme.js
@@ -0,0 +1,31 @@
+import { createTheme} from '@material-ui/core/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'montserrat, Roboto',
+  },
+  
+    palette: {
+      primary: {
+        // light: will be calculated from palette.primary.main,
+        main: '#e01836',
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contrast with palette.primary.main
+      },
+      secondary: {
+        light: '#4a9ca7',
+        main: '#3373b7',
+        // dark: will be calculated from palette.secondary.main,
+        contrastText: '#ffcc00',
+      },
+      // Used by `getContrastText()` to maximize the contrast between
+      // the background and the text.
+      contrastThreshold: 3,
+      // Used by the functions below to shift a color's luminance by approximately
+      // two indexes within its tonal palette.
+      // E.g., shift from Red 500 to Red 300 or Red 700.
+      tonalOffset: 0.2,
+    },
+  });
+
+export default theme;
